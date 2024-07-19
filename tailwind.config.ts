5b62ffd8eeb85b456a0712454b0ac32337bb6859
gugeorgy <gueigeorgy2@gmail.com>
2024-07-19 13:14:06 +0200

    :wrench: chore(theme): add theme configuration files.
    Integrate TailwindCSS with advanced type support for theme customization, including custom colors, typography and extended theme functionality.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import customTheme from './src/lib/theme';
 
 const config: Config = {
-  mode: "jit",
-  darkMode: ["class"],
+  mode: 'jit',
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
-  prefix: "",
-  theme: {},
+  prefix: '',
+  theme: {
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
+    extend: customTheme,
+  },
   plugins: [],
 };
 export default config;
