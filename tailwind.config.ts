f014346e1cea972f1ebe0e05177d374fac5278a6
gugeorgy <gueigeorgy2@gmail.com>
2024-07-19 12:36:32 +0200

    :tada: feat: initialize NextJS starter project with configs and dependencies
    This commit initializes the NextJS starter project, including essential configuration files and dependencies necessary for the project setup.

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  mode: "jit",
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {},
+  plugins: [],
+};
+export default config;
