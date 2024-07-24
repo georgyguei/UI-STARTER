0dfcfad71a62323f88ae074dd80525f2920a97fb
gugeorgy <gueigeorgy2@gmail.com>
2024-07-24 11:54:36 +0200

    :sparkles: feat(components/ui/form): enhance FormControl component with formId generation
    Improve the FormControl component by generating a unique formId for each instance. This ensures proper association between form elements and their labels, error messages, and help texts. This enhancement enhances form accessibility and user experience.

--- src/components/ui/form/control/index.tsx
+++ src/components/ui/form/control/index.tsx
@@ -4,6 +4,12 @@ import Box from '../../layout/box';
 import type { UIComponent } from '../../type';
 import FormRequiredIndicator from './indicator';
 
+const generateNumberIdTwoDigits = (arg0: string) => {
+  const randomNumber = Math.floor(Math.random() * 100);
+  const twoDigitNumber = randomNumber.toString().padStart(2, '0');
+  return arg0 + twoDigitNumber;
+};
+
 export type FormControlProps = {
   isInvalid?: boolean;
   isRequired?: boolean;
@@ -33,6 +39,34 @@ const FormControl: UIComponent<'div', FormControlProps> = props => {
     ...rest
   } = props;
 
+  const formId = generateNumberIdTwoDigits('field-:r');
+
+  const formHelpTextElement = Children.map(children, child => {
+    if (
+      isValidElement(child) &&
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      (child as any).type.displayName === 'FormHelpText'
+    ) {
+      return cloneElement(child as React.ReactElement, {
+        id: `${formId}-help-text`,
+      });
+    }
+  });
+
+  const formErrorMessageElement = isInvalid
+    ? Children.map(children, child => {
+        if (
+          isValidElement(child) &&
+          // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+          (child as any).type.displayName === 'FormErrorMessage'
+        ) {
+          return cloneElement(child as React.ReactElement, {
+            id: `${formId}-feedback`,
+          });
+        }
+      })
+    : undefined;
+
   const formRequiredIndicatorElement = isRequired
     ? Children.toArray(children).find(child => {
         if (isValidElement(child)) {
@@ -53,9 +87,31 @@ const FormControl: UIComponent<'div', FormControlProps> = props => {
             isDisabled,
             isReadOnly,
             requiredIndicator: formRequiredIndicatorElement,
+            id: `${formId}-label`,
+            htmlFor: formId,
           });
+        case 'Input': {
+          const ariaDescribedBy = [
+            isInvalid ? `${formId}-feedback` : '',
+            formHelpTextElement ? `${formId}-help-text` : '',
+          ]
+            .join(' ')
+            .trim();
+
+          return cloneElement(child as React.ReactElement, {
+            isInvalid,
+            isDisabled,
+            isReadOnly,
+            id: formId,
+            'aria-describedby': ariaDescribedBy,
+          });
+        }
+        case 'FormHelpText':
+          return formHelpTextElement;
+        case 'FormErrorMessage':
+          return formErrorMessageElement;
         case 'FormRequiredIndicator':
-          return null;
+          return undefined;
       }
     }
     return child;
